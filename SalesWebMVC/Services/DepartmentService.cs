9fe358371aa7d8b1001773d186101a51346c1a26
Alex Pierini <alex_pierini@hotmail.com>
2020-07-05 16:35:16 -0400

    Implementing grouping search

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 
-namespace SalesWebMvc.Services
+namespace SalesWebMVC.Services
 {
     public class DepartmentService
     {
